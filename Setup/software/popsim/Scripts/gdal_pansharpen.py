b5c987f0edc8ea32783dfba4b80c2f47f2864c29
Aditya Gore <aditya.gore@rsginc.com>
2023-11-22 15:04:17 -0500

    Missed some populationsim python files. Added those in this commit

---
+++ Setup/software/popsim/Scripts/gdal_pansharpen.py
@@ -0,0 +1,4 @@
+#!D:\bld\gdal-split_1631551144453\_h_env\python.exe
+# EASY-INSTALL-SCRIPT: 'GDAL==3.3.2','gdal_pansharpen.py'
+__requires__ = 'GDAL==3.3.2'
+__import__('pkg_resources').run_script('GDAL==3.3.2', 'gdal_pansharpen.py')
