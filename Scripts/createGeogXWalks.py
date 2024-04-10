a47fb30eb9ca5387a4411f2461d89f61f0747298
Aditya Gore <aditya.gore@rsginc.com>
2024-04-10 09:51:17 -0400

    Updated the scripts to work with latest PUMS data i.e. 2022 issue #3

--- Scripts/createGeogXWalks.py
+++ Scripts/createGeogXWalks.py
@@ -11,11 +11,12 @@ USER_DIR = os.path.join(os.getcwd(),'Data', 'USER')
 
 maz = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'MAZ_DIR']['Value'].item().strip(),'MZ_nashville_clean.shp'))[['ID','geometry']]
 taz = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'TAZ_DIR']['Value'].item().strip(),'TAZ_nashville_split_project.shp'))[['ID','geometry']]
-tract = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'CT_DIR']['Value'].item().strip(),'nashville_tract.shp'))[['GEOID','geometry']]
-bg = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'BG_DIR']['Value'].item().strip(),'nashville_bg.shp'))[['GEOID','geometry']]
-puma = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'PUMA_DIR']['Value'].item().strip(),'nashville_puma.shp'))[['GEOID10','geometry']]
-county = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'CNTY_DIR']['Value'].item().strip(),'nashville_county.shp'))[['GEOID','geometry']]
-gq_pumas = pd.read_csv(os.path.join(USER_DIR, 'gq_pumas.csv'))
+tract = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'CT_DIR']['Value'].item().strip(),'tl_2022','nashville_tract.shp'))[['GEOID','geometry']]
+bg = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'BG_DIR']['Value'].item().strip(),'tl_2022','nashville_bg.shp'))[['GEOID','geometry']]
+puma = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'PUMA_DIR']['Value'].item().strip(),'nashville_newpuma.shp'))[['PUMA','geometry']].rename(columns={'PUMA':'GEOID10'})
+county = gpd.read_file(os.path.join(parameters.loc[parameters.Key == 'CNTY_DIR']['Value'].item().strip(),'tl_2022','nashville_county.shp'))[['GEOID','geometry']]
+# gq_pumas = pd.read_csv(os.path.join(USER_DIR, 'gq_pumas.csv'))
+maz_controls = pd.read_csv(os.path.join(USER_DIR, 'control_totals_maz.csv'))
 
 maz = maz.to_crs(taz.crs)
 tract = tract.to_crs(taz.crs)
@@ -34,6 +35,10 @@ maz_taz['area'] = maz_taz.area
 ids_largest_area = maz_taz.groupby('MAZ', as_index=True)['area'].idxmax()
 x_walk_0 = maz_taz.loc[ids_largest_area.values, ['MAZ', 'TAZ']].sort_values('MAZ')
 
+# Confirm all the MAZ and TAZ are present
+print("MAZ Before {}, MAZ After {}".format(maz.shape[0], len(x_walk_0.MAZ.unique())))
+print("TAZ Before {}, TAZ After {}".format(taz.shape[0], len(x_walk_0.TAZ.unique())))
+
 # taz['point'] = taz.geometry.map(lambda n: n.representative_point())
 # taz['geometry'] = taz['point']
 
@@ -44,6 +49,11 @@ taz_bg = gpd.overlay(taz, bg, how='intersection').rename(columns={'ID':'TAZ', 'G
 taz_bg['area'] = taz_bg.area
 ids_largest_area = taz_bg.groupby('TAZ', as_index=True)['area'].idxmax()
 x_walk_1 = taz_bg.loc[ids_largest_area.values, ['TAZ', 'BLOCKGRP']].sort_values('TAZ')
+
+
+# Confirm all the BlockGroups and TAZ are present
+print("BG Before {}, BG After {}".format(bg.shape[0], len(x_walk_1.BLOCKGRP.unique())))
+print("TAZ Before {}, TAZ After {}".format(taz.shape[0], len(x_walk_1.TAZ.unique())))
 
 # bg['point'] = bg.geometry.map(lambda n:n.representative_point())
 # bg['geometry'] = bg['point']
@@ -56,6 +66,10 @@ bg_tract['area'] = bg_tract.area
 ids_largest_area = bg_tract.groupby('BLOCKGRP', as_index=True)['area'].idxmax()
 x_walk_2 = bg_tract.loc[ids_largest_area.values, ['BLOCKGRP', 'TRACT']].sort_values('BLOCKGRP')
 
+# Confirm all the BlockGroups and Tract are present
+print("BG Before {}, BG After {}".format(bg.shape[0], len(x_walk_2.BLOCKGRP.unique())))
+print("Tract Before {}, Tract After {}".format(tract.shape[0], len(x_walk_2.TRACT.unique())))
+
 # tract['point'] = tract.geometry.map(lambda n: n.representative_point())
 # tract.geometry = tract['point']
 
@@ -66,6 +80,11 @@ tract_puma = gpd.overlay(tract, puma, how='intersection').rename(columns={'GEOID
 tract_puma['area'] = tract_puma.area
 ids_largest_area = tract_puma.groupby('TRACT', as_index=True)['area'].idxmax()
 x_walk_3 = tract_puma.loc[ids_largest_area.values, ['TRACT', 'PUMA']].sort_values('TRACT')
+
+# Confirm all the BlockGroups and Tract are present
+print("PUMA Before {}, PUMA After {}".format(puma.shape[0], len(x_walk_3.PUMA.unique())))
+print("Tract Before {}, Tract After {}".format(tract.shape[0], len(x_walk_3.TRACT.unique())))
+
 
 # puma['point'] = puma.geometry.map(lambda n: n.representative_point())
 # puma.geometry = puma['point']
@@ -80,17 +99,21 @@ puma_county['area'] = puma_county.area
 ids_largest_area = puma_county.groupby('PUMA', as_index=True)['area'].idxmax()
 x_walk_4 = puma_county.loc[ids_largest_area.values, ['PUMA', 'COUNTY']].sort_values('PUMA')
 
+# Confirm all the BlockGroups and Tract are present
+print("PUMA Before {}, PUMA After {}".format(puma.shape[0], len(x_walk_4.PUMA.unique())))
+print("County Before {}, County After {}".format(county.shape[0], len(x_walk_4.COUNTY.unique())))
 
-x_walk_5 = x_walk_0.merge(x_walk_1, how = 'left', on = 'TAZ').merge(x_walk_2, how = 'left', on = 'BLOCKGRP').merge(x_walk_3, how = 'left', on = 'TRACT' ).merge(x_walk_4, how = 'left', on = 'PUMA')
+x_walk_5 = x_walk_0.merge(x_walk_1, how = 'left', on = 'TAZ').\
+    merge(x_walk_2, how = 'left', on = 'BLOCKGRP').\
+        merge(x_walk_3, how = 'left', on = 'TRACT' ).\
+            merge(x_walk_4, how = 'left', on = 'PUMA')
 x_walk_5['REGION'] = 3 # https://www2.census.gov/geo/pdfs/maps-data/maps/reference/us_regdiv.pdf
 x_walk_5.to_csv(os.path.join(parameters.loc[parameters.Key == 'XWALK_DIR']['Value'].item().strip(),'geo_crosswalks.csv'), index = None)
 
 #generate crosswalks for GQs. Only PUMA=47037 has GQs, so retain only this PUMA in the crosswalk
-print(gq_pumas.head())
-print(gq_pumas.dtypes)
-print(x_walk_5.head())
-print(x_walk_5.dtypes)
-x_walk_6 = x_walk_5[x_walk_5.PUMA.astype(int).isin(gq_pumas.PUMA)]
+maz_controls = maz_controls[maz_controls.GQ>0]
+gq_pumas = x_walk_5[x_walk_5.MAZ.isin(maz_controls.MAZ)].PUMA.unique()
+x_walk_6 = x_walk_5[x_walk_5.PUMA.isin(gq_pumas)]
 print(x_walk_6.head())
 x_walk_6.to_csv(os.path.join(parameters.loc[parameters.Key == 'XWALK_DIR']['Value'].item().strip(),'geo_crosswalksGQ.csv'), index = None)
 
--- Scripts/geo_process.py
+++ Scripts/geo_process.py
@@ -1,13 +1,23 @@
+from Scripts.buildControls import USER_DIR
 import geopandas as gpd
+import numpy as np
 import os
-directory = r'E:\Projects\Clients\NashvilleMPO\ModelUpdate2023\Tasks\Task4_Enhancements\Update_PopulationSim_Software\GitHub_William\Data'
-taz = gpd.read_file(r'E:\Projects\Clients\NashvilleMPO\ModelUpdate2023\Tasks\Task4_Enhancements\Update_PopulationSim_Software\GitHub_William\Data\TAZ\TAZ_nashville_split_project.shp')
+directory = r'.\Data'
+taz = gpd.read_file(r'.\Data\TAZ\TAZ_nashville_split_project.shp')
 taz_union = taz.unary_union
+USER_DIR = os.path.join(os.getcwd(),'Data', 'USER')
 
-def within_taz(df):
-	df['point'] = df.geometry.map(lambda n: n.representative_point())
-	df['in'] = df['point'].map(lambda l: l.within(taz_union))
-	return df.drop('point', axis = 1)
+def within_taz(df, PUMAFIELD='PUMACE10'):
+	df_filtered = df.loc[df.intersects(taz_union)]
+	df_filtered['AREA'] = df_filtered.to_crs(crs=3857).area
+	df_filtered = df_filtered.overlay(taz[['geometry']].dissolve(),how='intersection', keep_geom_type=True)
+	df_filtered['INTAREA'] = df_filtered.to_crs(crs=3857).area
+	df_filtered['PROP'] = df_filtered['INTAREA']/df_filtered['AREA']
+	df_filtered = df_filtered.loc[df_filtered.PROP>.05]
+	# df['point'] = df.geometry.map(lambda n: n.representative_point())
+	# df['in'] = df['point'].map(lambda l: l.within(taz_union))
+	df['in'] = df[PUMAFIELD].isin(df_filtered[PUMAFIELD])
+	return df
 	
 bg = gpd.read_file(os.path.join(directory,'BlockGroup','tl_rd22_47_bg.shp'))
 bg = bg.to_crs(taz.crs)
@@ -28,8 +38,44 @@ county = within_taz(county)
 #county[(county['in']) | (county.index == 2405)].drop('in',axis = 1).to_file(filename = os.path.join(directory,'County','nashville_county.shp'), driver = 'ESRI Shapefile')
 county[county['in']==1].drop('in',axis = 1).to_file(filename = os.path.join(directory,'County','nashville_county.shp'), driver = 'ESRI Shapefile')
 
-puma = gpd.read_file(os.path.join(directory,'PUMS','Geography','tl_2022_47_puma20.shp'))
-puma = puma.to_crs(taz.crs)
-puma = within_taz(puma)
+puma20 = gpd.read_file(os.path.join(directory,'PUMS','Geography', 'tl_2022','tl_2022_47_puma20.shp'))
+puma10 = gpd.read_file(os.path.join(directory,'PUMS','Geography','tl_2021_47_puma10.shp'))
+
+puma20 = puma20.to_crs(taz.crs)
+puma10 = puma10.to_crs(taz.crs)
+puma20 = within_taz(puma20, PUMAFIELD='PUMACE20')
+puma10 = within_taz(puma10, PUMAFIELD='PUMACE10')
+puma20 = puma20[puma20['in']==1].drop('in',axis = 1)
+puma10 = puma10[puma10['in']==1].drop('in',axis = 1)
+puma20['POLYAREA20'] = puma20.to_crs(crs=3857).area
+puma10['POLYAREA10'] = puma10.to_crs(crs=3857).area
+
+puma = puma10.overlay(puma20, how='union', keep_geom_type=True)
+puma = puma.explode().reset_index(drop=True)
+puma['PUMA'] = puma['PUMACE20'].astype(str) + puma['PUMACE10'].astype('str')
+puma['PUMA'] = puma['PUMA'] + puma.groupby('PUMA', as_index=False)['PUMA'].transform('cumcount').astype('str')
+puma['INTSECTIONAREA'] = puma.to_crs(crs=3857).area
+puma['PROP10'] = puma['INTSECTIONAREA']/puma['POLYAREA10']
+puma['PROP20'] = puma['INTSECTIONAREA']/puma['POLYAREA20']
+puma.groupby(['PUMACE10','POLYAREA10'],as_index=False)['INTSECTIONAREA'].sum().astype(np.int64)
+puma.groupby(['PUMACE20','POLYAREA20'],as_index=False)['INTSECTIONAREA'].sum().astype(np.int64)
+puma.loc[puma.PUMACE10.isna(),'PROP20'] = 0.0
+puma.loc[puma.PUMACE20.isna(),'PROP10'] = 0.0
+puma.groupby(['PUMACE10','POLYAREA10'],as_index=False)['PROP10'].sum()
+puma.groupby(['PUMACE20','POLYAREA20'],as_index=False)['PROP20'].sum()
+puma = puma.loc[(~puma.PUMACE10.isna()) & (~puma.PUMACE20.isna())]
+puma['NEWPROP10'] = puma.groupby('PUMACE10')['PROP10'].transform(lambda x: x/sum(x))
+puma['NEWPROP20'] = puma.groupby('PUMACE20')['PROP20'].transform(lambda x: x/sum(x))
+puma = puma.loc[(puma.NEWPROP10 > 1E-2) | (puma.NEWPROP20 > 1E-2)]
+puma['NEWPROP10'] = puma.groupby('PUMACE10')['PROP10'].transform(lambda x: x/sum(x))
+puma['NEWPROP20'] = puma.groupby('PUMACE20')['PROP20'].transform(lambda x: x/sum(x))
+puma['PROP10'] = puma['NEWPROP10']
+puma['PROP20'] = puma['NEWPROP20']
+puma = puma.drop(['NEWPROP20', 'NEWPROP10'], axis=1)
+
 #puma[(puma['in']) | (puma.index.isin([9,25]))].drop('in',axis = 1).to_file(filename = os.path.join(directory,'PUMS','Geography','nashville_puma.shp'), driver = 'ESRI Shapefile')
-puma[puma['in']==1].drop('in',axis = 1).to_file(filename = os.path.join(directory,'PUMS','Geography','nashville_puma.shp'), driver = 'ESRI Shapefile')
+# puma[puma['in']==1].drop('in',axis = 1).to_file(filename = os.path.join(directory,'PUMS','Geography','nashville_puma.shp'), driver = 'ESRI Shapefile')
+puma[['PUMACE10','PUMACE20','PUMA','PROP10','PROP20','POLYAREA10','POLYAREA20','INTSECTIONAREA','geometry']].\
+	to_file(filename = os.path.join(directory,'PUMS','Geography','nashville_newpuma.shp'), driver = 'ESRI Shapefile')
+
+puma[['PUMACE10','PUMACE20','PUMA','PROP10','PROP20','POLYAREA10','POLYAREA20','INTSECTIONAREA']].to_csv(os.path.join(USER_DIR, 'puma20_puma10_newpuma_xwalk.csv'))
